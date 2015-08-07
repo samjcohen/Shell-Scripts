3340dc35ec432ec5c619f88a6d3a067be6a33530
samjcohen <scohen93@gmail.com>
2015-08-07 13:00:23 -0700

    Create Macports AutoUpdate.sh

---
+++ Macports AutoUpdate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  Macports AutoUpdate.sh
+#  Created by Sam Cohen on 8/6/15.
+
+sudo port selfupdate > /dev/null
+echo "Repository up-to-date"
+if sudo port outdated > /dev/null; then
+  sudo port upgrade outdated
+  sudo port uninstall inactive > /dev/null # If lots of compiling nessessary, you may have to reenter admin passsword here
+  echo "Removing old ports"
+  echo "All software up-to-date"
+else
+  echo "No updates necessary" # No other action taken
+fi
+return 0
