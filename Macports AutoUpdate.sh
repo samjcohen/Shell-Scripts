219ad2c1b6e5eb4e1ab72a7808ee7ddb79d548ae
samjcohen <samjcohen@users.noreply.github.com>
2015-09-10 23:37:09 -0700

    Cleanup
    Got rid of redundant code

--- Macports AutoUpdate.sh
+++ Macports AutoUpdate.sh
@@ -5,12 +5,8 @@
 
 sudo port selfupdate > /dev/null
 echo "Repository up-to-date"
-if sudo port outdated > /dev/null; then
-    sudo port upgrade outdated
-    sudo port uninstall inactive > /dev/null # If lots of compiling nessessary, you may have to reenter admin passsword here
-    echo "Removing old ports"
-    echo "All software up-to-date"
-else
-    echo "No updates necessary" # No other action taken
-fi
+sudo port upgrade outdated
+sudo port uninstall inactive > /dev/null # If lots of compiling nessessary, you may have to reenter admin passsword here
+echo "Removing old ports"
+echo "All software up-to-date"
 exit 0
